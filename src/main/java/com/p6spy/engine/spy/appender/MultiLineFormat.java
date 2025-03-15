0d003fc91cb1cfe288020f5075cb2464102cfd67
llupin <parkyongkil@gmail.com>
2025-03-15 14:54:49 +0900

    출력 쿼리에서 빈줄삭제

--- src/main/java/com/p6spy/engine/spy/appender/MultiLineFormat.java
+++ src/main/java/com/p6spy/engine/spy/appender/MultiLineFormat.java
@@ -4,6 +4,7 @@ public class MultiLineFormat implements MessageFormattingStrategy {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         /* Slf4J 로거를 사용할 때는, Slf4J 에서 시간을 출력하므로 now 삭제 */
+        sql = sql.replaceAll("(?:[ \\t]*(\\R))+", "$1");
         return "# took " + elapsed + "ms | " + category + " | connection " + connectionId + "| url " + url + "\n" + sql + ";";
     }
 }
